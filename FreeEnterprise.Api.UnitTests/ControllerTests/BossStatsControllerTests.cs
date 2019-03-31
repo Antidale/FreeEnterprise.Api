cde04502bdac14a3f7033f169602bffb21d6d684
Scott Wyngarden <swyngarden@gmail.com>
2019-03-30 19:22:57 -0700

    adding unit test project and a starter test

---
+++ FreeEnterprise.Api.UnitTests/ControllerTests/BossStatsControllerTests.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using NUnit.Framework;
+using Moq;
+using FreeEnterprise.Api.Interfaces;
+using FreeEnterprise.Api.Controllers;
+using FreeEnterprise.Api.Requests;
+using FreeEnterprise.Api.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FreeEnterprise.Api.UnitTets.ControllerTests
+{
+	public class BossStatsControllerTests
+	{
+		Mock<IBossStatsRepository> bossStatsRepositoryMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			bossStatsRepositoryMock = new Mock<IBossStatsRepository>();
+
+		}
+
+		[TestCase(0, 0, HttpStatusCode.BadRequest)]
+		public async Task BossStatsController_Requires_ValidRequest(int locationId, int battleId, HttpStatusCode responseCode)
+		{
+			bossStatsRepositoryMock
+				.Setup(x => x.SearchAsync(It.IsAny<BossStatsSearchRequest>()))
+				.ReturnsAsync(new List<BossStat>());
+
+			var sut = new BossStatsController(bossStatsRepositoryMock.Object);
+
+			var request = new BossStatsSearchRequest
+			{
+				LocationId = locationId,
+				BattleId = battleId
+			};
+
+			var response = await sut.Search(request);
+			Assert.AreEqual((HttpStatusCode)(response.Result as StatusCodeResult).StatusCode, responseCode);
+
+		}
+	}
+}

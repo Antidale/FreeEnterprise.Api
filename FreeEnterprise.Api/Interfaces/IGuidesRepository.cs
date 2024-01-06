d5676e85a8f438c8b5ae5d5601d1c95bbc2f921f
antidale <antidale@gmail.com>
2024-01-05 16:42:27 -0800

    Adding ability to search for guides

---
+++ FreeEnterprise.Api/Interfaces/IGuidesRepository.cs
@@ -0,0 +1,10 @@
+﻿using FeInfo.Common.DTOs;
+using FreeEnterprise.Api.Classes;
+
+namespace FreeEnterprise.Api.Interfaces
+{
+    public interface IGuidesRepository
+    {
+        Task<Response<IEnumerable<Guide>>> GetGuidesAsync(string searchText);
+    }
+}

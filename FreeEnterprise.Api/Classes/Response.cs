c324234f0fba205c4c7554720b54a98426583993
Antidale <antidale@tellah.life>
2024-11-07 17:48:23 -0800

    Add ability for users to update their own pronouns through the bot

--- FreeEnterprise.Api/Classes/Response.cs
+++ FreeEnterprise.Api/Classes/Response.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace FreeEnterprise.Api.Classes
 {
@@ -74,6 +75,44 @@ namespace FreeEnterprise.Api.Classes
             (false, HttpStatusCode.Conflict) => new ConflictObjectResult(ErrorMessage),
             (false, HttpStatusCode.InternalServerError) => throw new InvalidOperationException(ErrorMessage),
             _ => new UnprocessableEntityObjectResult(ErrorMessage),
+        };
+    }
+
+    public class Response
+    {
+        private string _errorMessage = string.Empty;
+        private bool _success = false;
+        private HttpStatusCode? _errorStatusCode;
+        
+        public Response SetSuccess()
+        {
+            _success = true;
+            _errorStatusCode = null;
+            _errorMessage = string.Empty;
+            return this;
+        }
+        public Response BadRequest(string errorMessage) => SetError(errorMessage, HttpStatusCode.BadRequest);
+        public Response Unauthorized(string errorMessage) => SetError(errorMessage, HttpStatusCode.Unauthorized);
+        public Response NotFound(string errorMessage) => SetError(errorMessage, HttpStatusCode.NotFound);
+        public Response Conflict(string errorMessage) => SetError(errorMessage, HttpStatusCode.Conflict);
+        public Response InternalServerError(string errorMessage) => SetError(errorMessage, HttpStatusCode.InternalServerError);
+
+        private Response SetError(string errorMessage, HttpStatusCode errorStatusCode = HttpStatusCode.InternalServerError)
+        {
+            _errorMessage = errorMessage;
+            _success = false;
+            _errorStatusCode = errorStatusCode;
+            return this;
+        }
+        public ActionResult GetRequestResponse() => (_success, _errorStatusCode) switch
+        {
+            (true, _) => new NoContentResult(),
+            (false, HttpStatusCode.BadRequest) => new BadRequestObjectResult(_errorMessage),
+            (false, HttpStatusCode.Unauthorized) => new UnauthorizedObjectResult(_errorMessage),
+            (false, HttpStatusCode.NotFound) => new NotFoundObjectResult(_errorMessage),
+            (false, HttpStatusCode.Conflict) => new ConflictObjectResult(_errorMessage),
+            (false, HttpStatusCode.InternalServerError) => throw new InvalidOperationException(_errorMessage),
+            _ => new UnprocessableEntityObjectResult(_errorMessage),
         };
     }
 }

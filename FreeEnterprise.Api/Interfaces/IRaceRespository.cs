1ae4ab538dfcde568fc376900407f382a2fc87c2
Antidale <antidale@tellah.life>
2025-07-31 17:19:58 -0700

    Finishing endpoints for pulling the patch page html

--- FreeEnterprise.Api/Interfaces/IRaceRespository.cs
+++ FreeEnterprise.Api/Interfaces/IRaceRespository.cs
@@ -8,5 +8,6 @@ public interface IRaceRespository
     Task<Response<int>> CreateRaceAsync(CreateRaceRoom createRequest);
     Task<Response<IEnumerable<RaceDetail>>> GetRacesAsync(int offset, int limit, string? description, string? flagset);
     Task<Response<RaceDetail>> GetRaceAsync(string idOrSlug);
+    Task<Response<string>> GetRaceSeedHtmlAsync(string idOrSlug);
     Task<Response> JoinRaceAsync(JoinRaceRequest joinRaceRequest);
 }

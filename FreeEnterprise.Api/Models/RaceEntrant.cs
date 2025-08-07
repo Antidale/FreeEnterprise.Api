4f7c26b6f5fe41cdf7251b998138787e9f33c92f
Antidale <antidale@tellah.life>
2025-08-06 20:37:17 -0700

    WIP for Racers endpoints and /entrants stuff

--- FreeEnterprise.Api/Models/RaceEntrant.cs
+++ FreeEnterprise.Api/Models/RaceEntrant.cs
@@ -4,5 +4,13 @@ namespace FreeEnterprise.Api.Models;
 
 public class RaceEntrant
 {
+#pragma warning disable IDE1006 // Naming Styles - ignoring for pure database models, these names reflect what is in the database
+    public int race_id { get; set; }
+    public int entrant_id { get; set; }
+    public TimeSpan? finish_time { get; set; }
+    public int? placement { get; set; }
+    public Dictionary<string, string> metadata { get; set; } = [];
 
+
+#pragma warning restore IDE1006 // Naming Styles - ignoring for pure database models, these names reflect what is in the database
 }
--- FreeEnterprise.Api/Models/Racer.cs
+++ FreeEnterprise.Api/Models/Racer.cs
@@ -4,5 +4,13 @@ namespace FreeEnterprise.Api.Models;
 
 public class Racer
 {
+#pragma warning disable IDE1006 // Naming Styles - ignoring for pure database models, these names reflect what is in the database
+    public int id { get; set; }
+    public string discord_id { get; set; } = string.Empty;
+    public string discord_display_name { get; set; } = string.Empty;
+    public string racetime_id { get; set; } = string.Empty;
+    public string twitch_name { get; set; } = string.Empty;
 
+
+#pragma warning restore IDE1006 // Naming Styles - ignoring for pure database models, these names reflect what is in the database
 }

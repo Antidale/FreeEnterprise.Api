a778d636cc126a2c621723649bd887224840b3d6
Antidale <antidale@tellah.life>
2025-08-06 20:36:44 -0700

    Add RaceEntrant class and unit tests

---
+++ FreeEnterprise.Api.UnitTests/ClassesTests/RaceDetailTests.cs
@@ -0,0 +1,39 @@
+using System;
+using FluentAssertions;
+using FreeEnterprise.Api.Classes;
+
+namespace FreeEnterprise.Api.UnitTests.ClassesTests;
+
+public class RaceTests
+{
+    [Test]
+    public void RaceDetail_Metadata_Defaults_ToEmptyDictionary()
+    {
+        var sut = new RaceDetail();
+        sut.Metadata.Should().BeEmpty();
+    }
+
+    [Test]
+    public void RaceDetail_WithFilteredMetadata_RemovesFiterPrefix()
+    {
+        var presetMetadata = new Dictionary<string, string>
+        {
+            ["CR_RoomId"] = "1234",
+            ["Status"] = "Cancelled"
+        };
+
+        var expectedMetadata = new Dictionary<string, string>
+        {
+            ["Status"] = "Cancelled"
+        };
+
+        var sut = new RaceDetail
+        {
+            Metadata = presetMetadata
+        }.WithFilteredMetadata("CR_");
+
+        sut.Metadata.Should().NotBeEmpty();
+        sut.Metadata.Should().HaveCount(1);
+        sut.Metadata.Should().BeEquivalentTo(expectedMetadata);
+    }
+}
---
+++ FreeEnterprise.Api.UnitTests/ClassesTests/RaceEntrantTests.cs
@@ -0,0 +1,99 @@
+using FluentAssertions;
+using FreeEnterprise.Api.Classes;
+
+namespace FreeEnterprise.Api.UnitTests.ClassesTests;
+
+public class RaceEntrantTests
+{
+    [Test]
+    public void RaceEntrant_Defaults_ToEmptyDictioanry()
+    {
+        var sut = new RaceEntrant();
+        sut.Metadata.Should().BeEmpty();
+    }
+
+    [Test]
+    public void RaceEntrant_EnsureExpectedMetadata_AddsExpectedKeys_ToDefault()
+    {
+        var sut = new RaceEntrant().EnsureExpectedMetadata();
+        sut.Metadata.Should().NotBeEmpty();
+        sut.Metadata.Should().HaveCount(3);
+        sut.Metadata.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            ["score"] = "",
+            ["scoreChange"] = "",
+            ["comment"] = ""
+        });
+    }
+
+    [Test]
+    public void RaceEntrant_EnsureExpectedMetadata_ShouldNotOverride_ExistingSharedKeys()
+    {
+        var presetDictioanry = new Dictionary<string, string>
+        {
+            ["score"] = "1234",
+            ["scoreChange"] = "234",
+            ["comment"] = "oatly is made from oat-people"
+        };
+
+        var sut = new RaceEntrant
+        {
+            Metadata = presetDictioanry
+        }.EnsureExpectedMetadata();
+        sut.Metadata.Should().NotBeEmpty();
+        sut.Metadata.Should().HaveCount(3);
+        sut.Metadata.Should().BeEquivalentTo(presetDictioanry);
+    }
+
+    [Test]
+    public void RaceEntrant_EnsureExpectedMetadata_WillFillInMissingKeys()
+    {
+        var presetDictioanry = new Dictionary<string, string>
+        {
+            ["score"] = "1234",
+            ["comment"] = "oatly is made from oat-people"
+        };
+
+        var sut = new RaceEntrant
+        {
+            Metadata = presetDictioanry
+        }.EnsureExpectedMetadata();
+        sut.Metadata.Should().NotBeEmpty();
+        sut.Metadata.Should().HaveCount(3);
+        sut.Metadata.Should().BeEquivalentTo(new Dictionary<string, string>
+        {
+            ["score"] = "1234",
+            ["scoreChange"] = "",
+            ["comment"] = "oatly is made from oat-people"
+        });
+    }
+
+    [Test]
+    public void RaceEntrant_EnsureExpectedMetadata_DoesNotRemoveKeys()
+    {
+        var presetDictioanry = new Dictionary<string, string>
+        {
+            ["test_score"] = "1234",
+            ["test_scoreChange"] = "234",
+            ["test_comment"] = "oatly is made from oat-people"
+        };
+
+        var expectedDictionary = new Dictionary<string, string>
+        {
+            ["test_score"] = "1234",
+            ["test_scoreChange"] = "234",
+            ["test_comment"] = "oatly is made from oat-people",
+            ["score"] = "",
+            ["scoreChange"] = "",
+            ["comment"] = ""
+        };
+
+        var sut = new RaceEntrant
+        {
+            Metadata = presetDictioanry
+        }.EnsureExpectedMetadata();
+        sut.Metadata.Should().NotBeEmpty();
+        sut.Metadata.Should().HaveCount(expectedDictionary.Count);
+        sut.Metadata.Should().BeEquivalentTo(expectedDictionary);
+    }
+}

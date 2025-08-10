7d2f21a1d69225210b7ee7024569f76558aab13a
Antidale <antidale@tellah.life>
2025-08-10 16:41:38 -0700

    Adding start of more detailed documentation for the api

--- FreeEnterprise.Api/Classes/RaceEntrant.cs
+++ FreeEnterprise.Api/Classes/RaceEntrant.cs
@@ -7,10 +7,14 @@ public record RaceEntrant
     public string RacetimeId { get; set; } = string.Empty;
     public TimeSpan? FinishTime { get; set; }
     public int? Placement { get; set; }
+    /// <summary>
+    /// A colleciton of key/value pairs, both stored as strings. Always should contain the following keys: comment, score, scoreChange. Other keys, like status, may be present depending on other factors
+    /// </summary>
     public Dictionary<string, string> EntrantMetadata { get; set; } = [];
 
     /// <summary>
     /// Returns a new instance of this class, but ensures that score, scoreChanged, and comment all are present in the dictionary. If they were not previously included they have empty strings
+    /// </summary>
     /// <returns></returns>
     public RaceEntrant EnsureExpectedMetadata()
     {

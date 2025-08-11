ed6e7fcbcf9e14865246721c5b7548ccbc8db335
Antidale <antidale@tellah.life>
2025-08-11 13:53:49 -0700

    Adding xml docs for all races endpoints

--- FreeEnterprise.Api/Controllers/RacesController.cs
+++ FreeEnterprise.Api/Controllers/RacesController.cs
@@ -13,6 +13,13 @@ namespace FreeEnterprise.Api.Controllers
     {
         private readonly IRaceRespository _raceRepository = raceRespository;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="createRequest">A request detailing the required information for the race</param>
+        /// <returns>the internal id of the newly created race</returns>
+        /// <response code="200">In theory, this should be a 201 created, but is not.</response>
+        /// <response code="401">Requests without a valid api key get a 401 response</response>
         [ApiKey, HttpPost]
         public async Task<IActionResult> LogRaceCreatedAsync(CreateRaceRoom createRequest)
         {
@@ -20,6 +27,17 @@ namespace FreeEnterprise.Api.Controllers
             return insertResponse.GetRequestResponse();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="offset">The amount of records to skip. Combined with limit, lets you page through results</param>
+        /// <param name="limit">The amount of records to return. Limited to 500</param>
+        /// <param name="includeCancelled">By default, races with a status of 'cancelled' are excluded. set to true to also return these</param>
+        /// <param name="description">A word or phrase used in the race's description. If you're looking for races for a specific racer, use the /racers endoints instead.</param>
+        /// <param name="flagset">A portion of the flagset you want to search for. There are some oddities to this.</param>
+        /// <returns>A collection of RaceDetail objects. Unrecorded races are returned first, then in descending order of the race's finish time</returns>
+        /// <response code="200"></response>
+        /// <response code="400">Returned when requesting too much data</response> 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<RaceDetail>>> GetRacesAsync(
             [FromQuery] int offset = 0,
@@ -48,6 +66,13 @@ namespace FreeEnterprise.Api.Controllers
             return getResponse.GetRequestResponse();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="idOrSlug">The internal id (int)for the race, or the name of the racetime room.</param>
+        /// <returns>The detail for the specifc race, including entrants</returns>
+        /// <response code="200"></response>
+        /// <response code="404">Returned when no race can be found by the given id or name</response>
         [HttpGet("{idOrSlug}")]
         public async Task<ActionResult<RaceDetail>> GetRaceAsync(string idOrSlug)
         {
@@ -55,6 +80,14 @@ namespace FreeEnterprise.Api.Controllers
             return raceResponse.GetRequestResponse();
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="idOrSlug">The internal id (int) for the race, or the name of the racetime room.</param>
+        /// <returns>returns the entire html document to reconstruct the patch page.</returns>
+        /// <response code="200"></response>
+        /// <response code="404">Returned when no race can be found by the given id or name</response>
         [HttpGet("{idOrSlug}/seed")]
         public async Task<ActionResult<string>> GetRaceSeedAsync(string idOrSlug)
         {
